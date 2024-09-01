4c64bb820721b9b63840ac630fb57e253f858b49
Kisbogyi <kisbogyi2003@gmail.com>
2024-09-01 19:17:12 +0200

    [nvim] - added rustacean

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -2,17 +2,20 @@ return {
 	"williamboman/mason-lspconfig",
 	dependencies = {
 		{"williamboman/mason.nvim", opts = {} },
-			'neovim/nvim-lspconfig',
+		{'neovim/nvim-lspconfig' },
 	},
 	opts = {
 		ensure_installed = { 
 			"lua_ls",
 			"ruff_lsp",
+			"rust_analyzer",
 		},
 		handlers = {
 			function (server_name) -- default handler (optional)
 				require("lspconfig")[server_name].setup {}
 			end,
+           ["rust_analyzer"] = function ()
+           end,
 		},
 	},
 }
