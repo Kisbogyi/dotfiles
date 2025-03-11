351a5edf4f2c106ce0f04e46126ecf630e651990
Kisbogyi <kisbogyi2003@gmail.com>
2025-03-11 23:42:11 +0100

    add vim tmux config

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,3 +2,16 @@ require("config.lazy")
 require("settings")
 
 vim.cmd.colorscheme "catppuccin-mocha"
+
+-- use W command to save file with root
+vim.api.nvim_create_user_command('W', function()
+  vim.cmd('silent! write !sudo tee % > /dev/null')
+  vim.cmd('edit!')
+end, {})
+
+vim.cmd [[
+  highlight Normal guibg=none
+  highlight NonText guibg=none
+  highlight Normal ctermbg=none
+  highlight NonText ctermbg=none
+]]
